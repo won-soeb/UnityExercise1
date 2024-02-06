99ed9efdc26a7ac300de7cc352120eca4abd3c6f
wsinp431 <wsinp431@naver.com>
2024-02-06 11:10:31 +0900

    수정

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -7,17 +7,18 @@ using UnityEngine.UI;
 
 public class CubeController : MonoBehaviour
 {
-    private int count=0;
-    [SerializeField]private Button countBtn;
-    [SerializeField]private Button nextBtn;
-    [SerializeField]private Text countText;
+    private int count = 0;
+    [SerializeField] private Button countBtn;
+    [SerializeField] private Button nextBtn;
+    [SerializeField] private Text countText;
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
     private void Start()
     {
-        countBtn.onClick.AddListener(() => {
+        countBtn.onClick.AddListener(() =>
+        {
             ++count;
             countText.text = count.ToString();
         });
